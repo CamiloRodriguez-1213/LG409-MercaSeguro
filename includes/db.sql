4143b0abc48c95cb247af51240e517a688a8f093
Andres Camilo Rodriguez <andrescamilocr482@gmail.com>
2020-05-01 23:15:31 -0500

    Iniciando Proyecto

---
+++ includes/db.sql
@@ -0,0 +1,78 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         5.7.24 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para ventas_online
+CREATE DATABASE IF NOT EXISTS `ventas_online` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ventas_online`;
+
+-- Volcando estructura para tabla ventas_online.categorias
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla ventas_online.categorias: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` (`id`, `nombre`) VALUES
+	(1, 'Arte'),
+	(2, 'Alimentos'),
+	(3, 'Cuidado Personal'),
+	(4, 'Celulares y telefonos'),
+	(5, 'Computacion'),
+	(6, 'Electrodomesticos'),
+	(7, 'Deportes'),
+	(8, 'Tecnologia'),
+	(9, 'Musica y peliculas'),
+	(10, 'Otras categorias');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+
+-- Volcando estructura para tabla ventas_online.productos
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(20) NOT NULL DEFAULT '0',
+  `nombre` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  `precio` varchar(50) DEFAULT NULL,
+  `categoria` int(20) DEFAULT NULL,
+  `foto` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_productos_categorias` (`categoria`),
+  CONSTRAINT `FK_productos_categorias` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla ventas_online.productos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+
+-- Volcando estructura para tabla ventas_online.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(20) NOT NULL DEFAULT '0',
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(80) DEFAULT NULL,
+  `celular` varchar(20) DEFAULT NULL,
+  `whatsapp` varchar(20) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `ciudad` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla ventas_online.usuarios: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+    include('includes/verify_install.php');
+    include('includes/db.php');  
+    
+    $sql = "select * from usuarios";
+    
+    $result = DB::query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Last-Modified" content="0">
+    <link rel="stylesheet" type="text/css" href="complementos/inicio.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    <title>Inicio</title>
+    
+</head>
+<body>
+    
+    <div id="header-index" ><!--Header: Primera parte de la pagina-->
+    <div><a class="nav-logo" href="index.php" tabindex="2">Mercado Libre Colombia - Donde comprar y vender de todo</a></div>
+        <div >
+            <form class="form-search" action="" method="GET" role="search" >
+
+                <input type="text" class="search-input-index" aria-label="Ingresa lo que quieras encontrar" name="as_word" placeholder="Buscar productos, marcas y más…" maxlength="120" autofocus="" autocapitalize="off" autocorrect="off" spellcheck="false" autocomplete="off" >
+                
+                <button type="submit" class="btn btn-primary" >
+
+                    <div role="img" aria-label="Buscar" class="nav-icon-search"></div>
+            
+                </button>
+            </form>     
+        </div>
+        <div class="mini-header-index-2" ><!--Menu desplegable-->
+            <ul class="nav-index-1 nav-index-2-esp ">
+                <li><a href="">Categorias &#8744; </a>
+                    <ul class="mini-menu-config" >
+                        <a></a>
+                        <a href="">Vehículos</a>
+                        <li><a href="">Tecnología</a>
+                            <ul>
+                            <a href="">Tecnología</a>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="">Historial</a></li>
+                <li><a href="">Tiendas Oficiales</a></li>
+                <li><a href="">Ofertas de la semana</a></li>
+                <li><a href="">Vender</a></li>
+                <li><a href="">Ayuda/PQR</a></li>
+                
+            </ul>
+            <div >
+                <ul class="nav-categs" >
+                    <li class="esp"><a href="crear_usuario.php">Crear tu cuenta</a></li>
+                    <li><a href="">Ingresa</a></li>
+                    <li><a href="">Mis compras</a></li>
+                </ul>
+            </div>
+            
+        </div><!--Fin Menu desplegable-->
+        
+        
+    </div><!--Fin Header: Primera parte de la pagina-->
+       
+</body>
+</html>
