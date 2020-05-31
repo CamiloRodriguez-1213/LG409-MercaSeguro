0c297d8856409edca3d69b4a26e61ae6efac5323
william leguizamoh <williamleguizamon44@gmail.com>
2020-05-30 21:49:57 -0500

    Index productos

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ if ($_GET['pagina']==null || $_GET['pagina']==0 ||) {
 
 
 ?>
-
 <!doctype html>
 <html lang="es">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" type="text/css" href="css/estilo_inicio.css">
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -25,113 +25,127 @@ if ($_GET['pagina']==null || $_GET['pagina']==0 ||) {
   <link rel="stylesheet" type="text/css" href="../css/estilo.css">
   <script src='https://kit.fontawesome.com/a076d05399.js'></script>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning sticky-top row-12 sm-12 md-4">
-  
-  <h5><a class="navbar-brand ml-5" href="index.php">MercaSeguro </a></h5>
-    
-    
-    <ul class="navbar-nav ml-4 mr-2">
-    
-    
-    <form action="index.php?pagina=1" class="form-inline my-2 my-lg-0" method="GET">
-          <div class="row">
-            <div class="input-group">
-                <input class="form-control" type="text" name="busqueda" id="busqueda"  placeholder="Busca tus productos">
-                <input class="form-control" hidden type="text" name="pagina" id="pagina" value="1"  placeholder="Busca tus productos">
-                <span class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="submit" >
-                        <i class="fa fa-search"></i>
-                    </button>
-                </span>
+
+  <title>Mis Productos</title>
+  <?php include '../accesorios/navbar_plus.php' ?>
+</head>
+
+<body>
+  <div class="container my-5 justify-content-around" style="max-width: 100%">
+    <?php
+    $sql = "SELECT productos.id AS id_producto,estado,nombre_producto,descripcion_producto,precio,id_categoria_producto
+    ,imagen_producto,id_usuarios,estado,sub_cat_productos.id AS id_subcategoria,nombre_sub_producto,cat_productos.id 
+    AS id_categoria,nombre_cat_producto
+    FROM productos 
+            INNER JOIN sub_cat_productos
+            ON productos.id_categoria_producto = sub_cat_productos.id
+            INNER JOIN cat_productos
+            ON sub_cat_productos.id_cat_producto = cat_productos.id WHERE estado='activo'";
+    $result = DB::query($sql);
+    ?>
+    <div class="row row-12 justify-content-around">
+      <div class="d-flex flex-row">
+
+        <div class="p-2">
+          <div class="input-group">
+            <input class="form-input3" type="text" name="busqueda" id="busqueda" placeholder="Busca tus productos">
+            
+              <i class="fa fa-search resalta" style="cursor: pointer"></i>
+
+          </div>
+        </div>
+        <div class="p-2">
+          <h5><small><a href="">Activos</a></small></h5>
+        </div>
+        <div class="p-2">
+          <h5><small><a href="">Inactivos</a></small></h5>
+        </div>
+        <div class="p-2">
+          <h5><small><a href="">Todos</a></small></h5>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row ">
+      <!-- Solo se mirará en computador -->
+      <div class="d-none d-sm-none d-md-none d-lg-block">
+        <div class="col" style="width: 150px">
+          <div class="d-flex flex-column">
+
+            <div class="p-2">
+              <h5><small>Vendidos</small></h5>
+            </div>
+            <div class="p-2">
+              <h5><small>En espera</small></h5>
+            </div>
+            <div class="p-2">
+              <h5><small>Reclamos</small></h5>
             </div>
           </div>
-            
-          </form>
-          
-    </ul>
-  <?php 
-  
-  
+        </div>
+      </div>
 
-  include 'accesorios/navbar.php' ?>
-  </nav>
-</head>
-<body>
-  
+      <div class="col sm-12 justify-content-around">
+        <div class="row">
 
- 
-<div class="container">
-    <h1 class="my-5">Paginacion</h1>
-    <div class="row justify-content-around">
-      <?php
-      if (isset($_GET['busqueda'])) {
-        include 'procedimientos_externos/paginacion_buscar.php';
-   
-   while ($mostrar = mysqli_fetch_array($result_paginas)) : ?>
+          <?php
 
-     <div class="contenedor  sm-12 md-4">
-       <figure>
+          while ($mostrar = mysqli_fetch_array($result)) : ?>
 
-         <div class="card btn-light mr-2" style="width: 230px; height: 310px; ">
-           <div class="color">
-             <img class="zoom mt-3" src="data:image/jpg;base64,<?php echo base64_encode($mostrar['imagen_producto']) ?>" height="108rem" class="card-img-top" alt="OO">
-             <br><br>
-             <small>39% descuento</small>
-             <h3 class="card-text-success">$ <?php echo $mostrar['precio']; ?> </h3>
-             <p style="height: 50px; "><small> <?php echo $mostrar['nombre_producto']; ?> </small> </p>
-             <div class="capa">
-             
-             </div>
-           </div>
-         </div>
-       </figure>
-     </div>
-   <?php
-   endwhile;
-   include 'procedimientos_externos/barra_paginar_buscar.php';
-  
-      }
-      /* Cuando no se hace una busqueda */
-      if (!isset($_GET['busqueda'])&&(isset($_GET['pagina']))) {
-        include 'accesorios/carrusel.php';
-  
-        include 'procedimientos_externos/paginacion.php';
-        while ($mostrar = mysqli_fetch_array($result_paginas)) : ?>
+            <div class="contenedor">
 
-          <div class="contenedor  sm-12 md-4">
-            <figure>
-     
-              <div class="card btn-light mr-2" style="width: 230px; height: 310px; ">
-                <div class="color">
-                  <img class="zoom mt-3" src="data:image/jpg;base64,<?php echo base64_encode($mostrar['imagen_producto']) ?>" height="108rem" class="card-img-top" alt="OO">
-                  <br><br>
-                  <small>39% descuento</small>
-                  <h3 class="card-text-success">$ <?php echo $mostrar['precio']; ?> </h3>
-                  <p style="height: 50px; "><small> <?php echo $mostrar['nombre_producto']; ?> </small> </p>
-                  <div class="capa">
-                  
+
+              <div class="card my-3 ml-5 producto-hover <?php if ($mostrar['estado'] == "inactivo") { ?> producto_inactivo <?php } ?>" style="width: 240px; height: 360px;">
+
+                <div class="container ">
+                  <div class="capa btn-group navn dropleft my-3" style="float: right">
+
+                    <i class="fas fa-ellipsis-v resalta" style="width: 20px; height: 18px" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"></i>
+
+                    <div class="dropdown-menu mr-1" aria-labelledby="navbarDropdown">
+                      <form action="editar_producto.php" method="POST">
+                        <button class="dropdown-item" type="submit">Editar</button>
+                        <input type="text" hidden id="id_form_editar" name="id_form_editar" value="<?php echo $mostrar['id_producto'] ?>">
+                      </form>
+                      <form action="../procesos_productos/actualizar_producto.php" method="POST">
+                        <?php if ($mostrar['estado'] == "activo") {  ?>
+                          <button class="dropdown-item" type="submit">Inactivar</button>
+                          <input hidden id="id_producto" name="id_producto" value="<?php echo $mostrar['id_producto'] ?>">
+                          <input hidden id="estado" id="estado" name="estado" value="<?php echo $mostrar['estado'] ?>">
+                        <?php  } else {  ?>
+                          <button class="dropdown-item">Activar</button>
+                          <input hidden id="id_producto" name="id_producto" value="<?php echo $mostrar['id_producto'] ?>">
+                          <input hidden id="estado" id="estado" name="estado" value="<?php echo $mostrar['estado'] ?>">
+                        <?php  }  ?>
+
+                      </form>
+                      <form action="../procesos_productos/" method="post">
+                        <button class="dropdown-item" id="id_form_editar" name="id_form_editar" value="<?php echo $mostrar['id_producto'] ?>" type="submit">Eliminar</button>
+                      </form>
+
+                    </div>
+
                   </div>
+                  <img src="data:image/jpg;base64,<?php echo base64_encode($mostrar['imagen_producto']) ?>" style="height: 150px; width: 200px" class="card-img-top" alt="OO">
+                  <h3 class="card-text-success" style="height: 50px">$ <?php echo $mostrar['precio']; ?> </h3>
+                  <p style="height: 50px"><small> <?php echo $mostrar['nombre_producto']; ?> </small> </p>
+
                 </div>
               </div>
-            </figure>
-          </div>
-        <?php
-        endwhile;
-        include 'procedimientos_externos/barra_paginar.php';
-      ?>
-      
-      <?php
-    }
-      ?>
+
+
+            </div>
+          <?php
+          endwhile
+          ?>
+        </div>
+      </div>
     </div>
-  </div>
-  <br><br>
 
-  
 
-  <div style="float: right">
-    <p>© <?php echo date("Y"); ?> <a href="" target="_blank">MercaSeguro</a> Todos los derechos reservados | Diseñado por <a href="" target="_blank">MercaSeguro</a> </p>
   </div>
+
 </body>
 
 </html>
